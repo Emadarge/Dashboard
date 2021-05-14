6867dc88662c5a33e97bafc454cd9ea98919d096
Emadarge <74114494+Emadarge@users.noreply.github.com>
2021-05-14 11:46:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ django_heroku.settings(locals())
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']  
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)   
 
-app=app.server
+server=app.server
 
 df = pd.read_csv('raw_data_all_cleaned.csv')            
 
