962b89fd167c4ae380307611d1bbe9021eb30a4d
Emadarge <74114494+Emadarge@users.noreply.github.com>
2021-05-14 11:54:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import django_heroku
 import plotly.express as px
 from dash.dependencies import Input, Output
 import pandas as pd
@@ -15,7 +14,7 @@ import os
 from sklearn.cluster import KMeans
 
 
-django_heroku.settings(locals())
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']  
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)   
 
